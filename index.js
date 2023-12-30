0a668ff2094354818d3aa7e07823dcde8563a055
Michael Girgis <michael.girgis@live.ca>
2023-12-30 14:58:22 -0500

    if statement in index file fixed and shape class constructor changed

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 //packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Triangle, Circle, Square, } = require("./lib/shapes");
+
+function writeToFile(filename, answers) {
+    let svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+   
+  
+
+    if (answers.shape === "Triangle") {
+      let triangle = new Triangle(answers.shapeColour, answers.textColour, answers.text);
+      svgString += triangle.render();
+  
+    } else if (answers.shape === "Square") {
+      let square = new Square(answers.shapeColour, answers.textColour, answers.text);
+      svgString += square.render();
+    } else {
+      let circle = new Circle(answers.shapeColour, answers.textColour, answers.text);
+      svgString += circle.render();
+    }
+
+
+    fs.writeFile("logo.svg", svgString, (err) => {
+        err ? console.log(err) : console.log("Generated logo.svg");
+      });
+}
 
 //array of questions prompting user for input
 const questions = [
@@ -18,20 +43,20 @@ const questions = [
         type: 'list',
         name: 'shape',
         message: 'Select a shape for your logo.',
-        choices: ['Circle', 'Square', 'Trianle'],
+        choices: ['Circle', 'Square', 'Triangle'],
     },
     {
         type: 'input',
         message: 'What colour would you like the shape to be?',
         name: 'shapeColour',
     },
-]
+];
 
 //function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then(function(userInput) {
-        console.log(userInput);
+    .then(function(answers) {
+        writeToFile("logo.svg", answers);
     });
   };
 
